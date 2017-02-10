77fa429548580acf78066e782cc015a737007ccc
tstepourska <tstepourska@gmail.com>
2017-02-10 16:38:12 -0500

    Create Constants.java

---
+++ Constants.java
@@ -0,0 +1,17 @@
+package com.ts.test.util;
+
+public class Constants {
+	public static final int TICK_SIZE		= 5; //sec
+	public static final long MIN = 12;  // 1*12; 
+	public static final long HOUR =720; // 1*12*60; 
+	public static final long DAY = 17280;  //1*12*60*24; 
+	
+	public static final String TFU_MIN	= "MIN";
+	public static final String TFU_HOUR	= "HOUR";
+	public static final String TFU_DAY	= "DAY";
+	
+	public static final int STATUS_ERROR	 = 9;
+	
+	public static final long SLEEP_DF_INTERRUPTED	= 2000;
+	public static final long SLEEP_WAIT_FOR_BAR		= 5010;
+}
